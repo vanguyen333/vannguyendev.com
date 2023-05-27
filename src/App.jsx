f303810357dec965c9ed3f9f4729b152a5f7cdda
vanguyen333 <vanguyen333@yahoo.com>
2023-05-27 13:39:29 -0400

    displayed logo succesfully

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { BrowserRouter } from "react-router-dom";
 import {
   About,
+  Works,
   Contact,
   Experience,
   Feedbacks,
   Hero,
   Navbar,
   Tech,
-  Works,
   StarsCanvas,
 } from "./components";
 const App = () => {
