aaea62c1b748185393a3a98ac075f32b50f1e243
vanguyen333 <vanguyen333@yahoo.com>
2023-05-28 22:24:31 -0400

    switch back and forth with toggle and menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,6 +9,7 @@ import { logo1, menu, close } from "../assets";
 
 const Navbar = () => {
   const [active, setActive] = useState("");
+  const [toggle, setToggle] = useState(false);
 
   return (
     <nav className="{`{styles.paddindX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary}">
@@ -28,6 +29,29 @@ const Navbar = () => {
             <span className="sm:block hidden">Full Stack Developer</span>
           </p>
         </Link>
+        <ul className="list-none hidden sm:flex flex-row gap-10">
+          {navLinks.map((link) => (
+            <li
+              key={link.id}
+              className={`${
+                active === link.title ? "text-white" : "text-secondary"
+              }hover:text-white text-[18px] font-medium cursor-pointer`}
+              onClick={() => setActive(link.title)}
+            >
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
+        {/* Mobile navigation bar: About Work Contact */}
+        <div className="sm:hidden flex flex-1 justify-end items-center">
+          <img
+            src={toggle ? close : menu}
+            alt="menu"
+            className="w-[28px] height-[28px]
+            object-contain cursor-pointer"
+            onClick={() => setToggle(!toggle)}
+          />
+        </div>
       </div>
     </nav>
   );
